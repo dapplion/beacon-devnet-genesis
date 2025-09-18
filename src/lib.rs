84debd4bfa74b8e453b58e68d0482e7cf10fbc24
dapplion <35266934+dapplion@users.noreply.github.com>
2025-09-18 18:05:29 +0200

    revert clippy

--- src/lib.rs
+++ src/lib.rs
@@ -128,7 +128,7 @@ fn run_with_spec<T: EthSpec>(eth2_network_config: Eth2NetworkConfig, cli: &Cli) 
             };
 
             let completed = computed_validators.fetch_add(1, Ordering::Relaxed);
-            if completed.is_multiple_of(1000) {
+            if completed % 1000 == 0 {
                 eprintln!("computed validators {completed}")
             }
             Ok(validator)
